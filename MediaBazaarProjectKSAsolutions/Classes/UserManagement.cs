1fb3f356dc026a35c17003432026389fe35151fd
Andrija Hanga <a.hanga@student.fontys.nl>
2021-09-27 14:33:40 +0200

    Update

--- MediaBazaarProjectKSAsolutions/Classes/UserManagement.cs
+++ MediaBazaarProjectKSAsolutions/Classes/UserManagement.cs
@@ -66,20 +66,37 @@ namespace MediaBazaarProjectKSAsolutions.Classes
             {
                 using (MySqlConnection conn = new MySqlConnection(Params.connectionString))
                 {
-                    string sql = "select distinct(user.id)  from user";
-                    MySqlCommand cmd = new MySqlCommand(sql, conn);
-
                     conn.Open();
+                    User u = new User();
+                    string query = "select * from user where id=@id";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("id", id);
+                    
 
                     MySqlDataReader dr = (MySqlDataReader)cmd.ExecuteReader();
 
                     while (dr.Read())
                     {
-                        User user = new User();
-                        user.Id = id;
-                        
+
+                        u.Id = dr.GetInt32("id");
+                        u.UserName = dr.GetString("userName");
+                        u.FirstName = dr.GetString("firstName");
+                        u.LastName = dr.GetString("lastName");
+                        u.Email = dr.GetString("email");
+                        u.Password= dr.GetString("password");
+                        u.Birtyhday = dr.GetDateTime("birthday");
+                        u.BSN = dr.GetInt32("bSN");
+                        u.ZipCode = dr.GetString("zipCode");
+                        u.Address = dr.GetString("address");
+                        u.Gender = (Gender)dr.GetInt32("gender");
+                        u.Role = (Role)dr.GetInt32("role");
+                        u.Status = (Status)dr.GetInt32("status");
+                        return u;
+
                     }
-                    return null;// is not finished
+                    return null; 
+                    
                 }
             }
             catch (Exception)
