283443cf7973e99fbacbeccd155c6e3362bb945f
Andrija Hanga <a.hanga@student.fontys.nl>
2021-10-23 19:41:28 +0200

    Update

--- MediaBazaarProjectKSAsolutions/Classes/Contract.cs
+++ MediaBazaarProjectKSAsolutions/Classes/Contract.cs
@@ -12,5 +12,16 @@ namespace MediaBazaarProjectKSAsolutions.Classes
         public ContractType ContractType { get; set; }
         public double Salary { get; set; }
 
+        public Contract(int id,DateTime startDate,DateTime endDate, ContractType contractType,double salary)
+        {
+            this.ContractId = id;
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+            this.ContractType = contractType;
+            this.Salary = salary;
+        }
+
+        // every time we create user we should have boolean contract to say if that user is going to have a contract or not if yes add contract to user else nothing
+
     }
 }
