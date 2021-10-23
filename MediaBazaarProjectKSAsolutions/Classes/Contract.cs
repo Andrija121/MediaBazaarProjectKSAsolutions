09143a2c42150212fa543aa0dd54bb7b21ef7f58
Andrija Hanga <a.hanga@student.fontys.nl>
2021-10-23 19:36:14 +0200

    Implemented new User Status and class contract

---
+++ MediaBazaarProjectKSAsolutions/Classes/Contract.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediaBazaarProjectKSAsolutions.Classes
+{
+    class Contract
+    {
+        public int ContractId { get;}
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public ContractType ContractType { get; set; }
+        public double Salary { get; set; }
+
+    }
+}
