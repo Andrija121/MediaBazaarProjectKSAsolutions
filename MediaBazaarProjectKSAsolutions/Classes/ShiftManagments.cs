5d6928f36c69f67e522f786e8fe9b0cd4a985b3b
Boakye-Buah,Kenneth <kojo_buah@yahoo.com>
2021-11-02 23:23:13 +0100

    Crud For Shift

---
+++ MediaBazaarProjectKSAsolutions/Classes/ShiftManagments.cs
@@ -0,0 +1,171 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace MediaBazaarProjectKSAsolutions.Classes
+{
+    class ShiftManagments
+    {
+        MySqlConnection conn = new MySqlConnection(Params.connectionString);
+
+        public ShiftManagments()
+        {
+
+        }
+        public void AddShift( Shift shifts)  //This Adds Shifts
+        {
+            try
+            {
+                using(MySqlConnection conn = new MySqlConnection(Params.connectionString))
+                {
+                    string sql = "INSERT INTO shifts(user_id,shift_date,shift_type) VALUES(@User_Id,@Shift_date,Shift_Type)";
+
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                    conn.Open();
+                    cmd.Parameters.AddWithValue("@User_id", shifts.User_Id);
+                    cmd.Parameters.AddWithValue("@U", shifts.Shift_Date);
+                    cmd.Parameters.AddWithValue("@User_id", shifts.Shift_Type.ToString());
+                    cmd.ExecuteNonQuery(); 
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+        public Shift GetShiftId(int Shift_id ) //This Gets Shifts by ID
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(Params.connectionString))
+                {
+                    conn.Open();
+                    Shift shifts = new Shift();
+                    string query = "SELECT * FROM shift WHERE shift_id= @Shift_id";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("shift_Id", Shift_id);
+
+
+                    MySqlDataReader dr = (MySqlDataReader)cmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+
+                        shifts.Shift_Id = dr.GetInt32("shift_id");
+                        shifts.User_Id = dr.GetInt32("user_id");
+                        shifts.Shift_Date = dr.GetDateTime("shift_date");
+                        shifts.Shift_Type = (Shift_Type)dr.GetInt32("shift_type");
+
+                        return shifts;
+
+                    }
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public List<Shift> GetAllShifts()
+        {
+            try
+            {
+
+                using (MySqlConnection conn = new MySqlConnection(Params.connectionString))
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM Shift";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                    MySqlDataReader dr = (MySqlDataReader)cmd.ExecuteReader();
+
+                    List<Shift> shifts =new List<Shift>(); 
+
+                    while (dr.Read())
+                    {
+                        Shift shift = new Shift();
+                        shift.Shift_Id = Convert.ToInt32(dr["Shift_Id"]);
+                        shift.User_Id = Convert.ToInt32(dr["User_Id"]);
+                        shift.Shift_Date = Convert.ToDateTime(dr["Shift_Date"]);
+                        shift.Shift_Type = Enum.Parse<Shift_Type>(dr["Shift_Type"].ToString());
+                        shifts.Add(shift);
+                    }
+                    return shifts;
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }//This Returns AllShift
+        public Shift UpdateShifts(Shift shift) //This Updates Shift
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(Params.connectionString))
+                {
+                    conn.Open();
+                    string sql = "UPDATE shift SET User_Id =@User_Id, Shift_Date =@Shift_Date, Shift_Type=@Shift_Type";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                    cmd.Parameters.AddWithValue("@Shift_Id", shift.Shift_Id);
+                    cmd.Parameters.AddWithValue("@User_Id", shift.User_Id);
+                    cmd.Parameters.AddWithValue("@Shift_Date", shift.Shift_Date);
+                    cmd.Parameters.AddWithValue("@Shift_Type", shift.Shift_Type);
+
+                    cmd.ExecuteNonQuery();
+                }
+                return shift;
+                
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public void DeleteShift(int Shift_Id) //This Deletes Shift
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(Params.connectionString))
+                {
+                    string sql = "DELETE FROM shift WHERE Shift_Id =@Shift_Id";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@Shift_Id", Shift_Id);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ MediaBazaarProjectKSAsolutions/Forms/Schedule/FormSchedule.cs
@@ -0,0 +1,62 @@
+﻿using MediaBazaarProjectKSAsolutions.Classes;
+using MediaBazaarProjectKSAsolutions.Forms.Schedule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MediaBazaarProjectKSAsolutions.Forms
+{
+    public partial class FormSchedule : Form
+    {
+        ShiftManagments shm = new ShiftManagments();
+        public FormSchedule()
+        {
+            InitializeComponent();
+            RefreshListBox();
+        }
+
+        public void RefreshListBox()
+        {
+            lbSchedule.Items.Clear();
+            foreach(var shift in shm.GetAllShifts())
+            {
+                lbSchedule.Items.Add(shift);
+            }
+        }
+        private void btnRefreshSchedule_Click(object sender, EventArgs e)
+        {
+            RefreshListBox();
+        }
+
+        private void btnCreateSchedule_Click(object sender, EventArgs e)
+        {
+            CreateSchedule createSchedule = new CreateSchedule();
+            createSchedule.ShowDialog();
+            RefreshListBox();
+        }
+
+        private void btnEditSchedule_Click(object sender, EventArgs e)
+        {
+            Shift shift = (Shift)lbSchedule.SelectedItem;
+            if (shift != null)
+            {
+                EditSchedule editSchedule = new EditSchedule(shift);
+                editSchedule.ShowDialog();
+                RefreshListBox();
+
+
+            }
+            else
+                MessageBox.Show("Please slecet the shift you want to edit");
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
