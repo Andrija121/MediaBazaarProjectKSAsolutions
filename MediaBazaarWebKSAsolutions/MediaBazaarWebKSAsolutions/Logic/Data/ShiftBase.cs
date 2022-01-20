9a95e1d70392cc6d55f92583e49e312013809cfd
Sander Boers <i438165@fhict.nl>
2022-01-20 15:34:35 +0100

    wip

---
+++ MediaBazaarWebKSAsolutions/MediaBazaarWebKSAsolutions/Logic/Data/ShiftBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaBazaarWebKSAsolutions.Logic.Data
+{
+    public class ShiftBase
+    {
+    }
+}
--- MediaBazaarWebKSAsolutions/MediaBazaarWebKSAsolutions/Pages/Schedule.cshtml.cs
+++ MediaBazaarWebKSAsolutions/MediaBazaarWebKSAsolutions/Pages/Schedule.cshtml.cs
@@ -2,22 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using MediaBazaarProjectKSAsolutions.Classes;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MediaBazaarWebKSAsolutions.Pages
 {
-    public class ResupplyRequestModel : PageModel
+    public class ScheduleModel : PageModel
     {
         public string UsernameFromSession { get; set; }
-        RequestResupplyManagement rrm = new RequestResupplyManagement();
-        public List<ResupplyRequest> MyRRs { get; set; }
 
         public IActionResult OnGet()
         {
-            MyRRs = rrm.GetPendingResupplyRequests();
             string username = HttpContext.Session.GetString("username");
             if (username != null)
             {
